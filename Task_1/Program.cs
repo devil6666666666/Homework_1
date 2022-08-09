180f24edef121faaea9964be9d54944120e67081
Viktor A <abcd999@bk.ru>
2022-08-10 01:40:48 +0300

    Задача 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Нахождение большего числа из двух
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+}
+else if (a == b)
+{
+    Console.WriteLine("Числа равны");
+}
+else if (b > a)
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}
