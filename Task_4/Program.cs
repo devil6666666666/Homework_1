e505776167500611d60d818f9c779a13e7173e1d
Viktor A <abcd999@bk.ru>
2022-08-10 21:12:29 +0300

    Задача 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,33 @@
+﻿// Все чётные числа от 1 до N
+
+Console.WriteLine("Введите целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+if(N>0)
+{
+    while(a<=N)
+{
+    if(a%2==0)
+    {
+        Console.Write(a);
+        Console.Write(", ");
+    }
+    a=a+1;
+}
+}
+if(N<0)
+{
+    while(a>=N)
+{
+    if(a%2==0)
+    {
+        Console.Write(a);
+        Console.Write(", ");
+    }
+    a=a-1;
+}
+}
+if(N==0)
+{
+    Console.Write(N);
+}
