d167e696fdde5032fc01e0eec1fee925f0408921
Viktor A <abcd999@bk.ru>
2022-08-10 20:53:58 +0300

    Задача 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Является ли число четным
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
